d836f90127ad3997fefbc5e30e095842ac50fd24
SShipon <nxshipon@gmail.com>
2022-02-17 08:55:01 +0600

    hey

--- js/app.js
+++ js/app.js
@@ -7,4 +7,15 @@ function calculate() {
     const incomeInput = document.getElementById("income-filed").value;
     const incomeText = parseInt(incomeInput);
     console.log(incomeText);
+
+    // expense
+
+    const foodInput = document.getElementById('food-filed').value;
+    const rentInput = document.getElementById('rent-filed').value;
+    const clothesInput = document.getElementById('clothes-filed').value;
+    const calculateTotal =
+        parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothesInput);
+    const balanceTotal = incomeText - calculateTotal; 
+    document.getElementById('balance').innerText = balanceTotal;
+    document.getElementById('')
 }
